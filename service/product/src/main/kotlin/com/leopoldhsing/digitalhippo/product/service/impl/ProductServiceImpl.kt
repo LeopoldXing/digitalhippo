1ce3399d1ca877fc5a098e25de62f6a9b971223d
LeopoldHsing <LeopoldHsing@gmail.com>
2024-10-08 14:55:22 -0500

    feat: integrate with elasticsearch in product microservice

--- service/product/src/main/kotlin/com/leopoldhsing/digitalhippo/product/service/impl/ProductServiceImpl.kt
+++ service/product/src/main/kotlin/com/leopoldhsing/digitalhippo/product/service/impl/ProductServiceImpl.kt
@@ -1,19 +1,29 @@
 package com.leopoldhsing.digitalhippo.product.service.impl
 
+import com.leopoldhsing.digitalhippo.common.exception.ResourceNotFoundException
 import com.leopoldhsing.digitalhippo.model.dto.ProductSearchingConditionDto
 import com.leopoldhsing.digitalhippo.model.entity.Product
+import com.leopoldhsing.digitalhippo.product.repository.ProductRepository
 import com.leopoldhsing.digitalhippo.product.service.ProductService
+import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class ProductServiceImpl : ProductService {
+class ProductServiceImpl @Autowired constructor(
+    private val productRepository: ProductRepository
+) : ProductService {
 
     override fun conditionalSearchProducts(condition: ProductSearchingConditionDto): List<Product> {
         TODO("Not yet implemented")
     }
 
     override fun getProduct(productId: Long): Product {
-        TODO("Not yet implemented")
+        val product = productRepository.findByIdOrNull(productId)
+        if (product == null) {
+            throw ResourceNotFoundException("product", "id", productId.toString())
+        }
+        return product
     }
 
     override fun createProduct(product: Product): Product {
