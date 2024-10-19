087d7497d76bea388e6c68e6ec5de345177cce55
LeopoldHsing <LeopoldHsing@gmail.com>
2024-10-19 00:24:26 -0500

    build: update local deployment configuration

--- service/cart/src/main/kotlin/com/leopoldhsing/digitalhippo/cart/repository/CartRepository.kt
+++ service/cart/src/main/kotlin/com/leopoldhsing/digitalhippo/cart/repository/CartRepository.kt
@@ -3,11 +3,16 @@ package com.leopoldhsing.digitalhippo.cart.repository
 import com.leopoldhsing.digitalhippo.model.entity.Cart
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
+import org.springframework.transaction.annotation.Transactional
 
 @Repository
 interface CartRepository : JpaRepository<Cart, Long> {
     fun findCartsByUserId(userId: Long): Collection<Cart>
     fun findCartsByUserIdAndProductIdIn(userId: Long, productId: Collection<Long>): List<Cart>
+
+    @Transactional
     fun deleteCartByProductIdAndUserId(userId: Long, productId: Long)
+
+    @Transactional
     fun deleteCartsByUserId(userId: Long)
 }
