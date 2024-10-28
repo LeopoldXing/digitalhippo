f8087197f0f91d1ca0dc194c2fdee44944cdcf1b
LeopoldHsing <LeopoldHsing@gmail.com>
2024-10-27 22:18:22 -0500

    fix: working on fixing products not found error

--- service/product/src/main/kotlin/com/leopoldhsing/digitalhippo/product/config/BitmapInitialization.kt
+++ service/product/src/main/kotlin/com/leopoldhsing/digitalhippo/product/config/BitmapInitialization.kt
@@ -2,28 +2,45 @@ package com.leopoldhsing.digitalhippo.product.config
 
 import com.leopoldhsing.digitalhippo.common.constants.RedisConstants
 import com.leopoldhsing.digitalhippo.product.repository.ProductRepository
-import jakarta.annotation.PostConstruct
+import org.springframework.boot.context.event.ApplicationReadyEvent
+import org.springframework.context.ApplicationListener
 import org.springframework.context.annotation.Configuration
 import org.springframework.data.redis.core.StringRedisTemplate
+import org.springframework.util.CollectionUtils
 
 @Configuration
 open class BitmapInitialization constructor(
     private val redisTemplate: StringRedisTemplate,
     private val productRepository: ProductRepository
-) {
+) : ApplicationListener<ApplicationReadyEvent> {
+
+    /**
+     * making sure the springboot instance fully started
+     */
+    override fun onApplicationEvent(event: ApplicationReadyEvent) {
+        bitmapInitializer()
+    }
 
     /**
      * initialize product id bitmap
      */
-    @PostConstruct
     open fun bitmapInitializer() {
-        // 1. get all product ids
-        val productIds = productRepository.findAll().map { it.id }
+        try {
+            // 1. get all product ids
+            val productList = productRepository.findAll()
+            if (CollectionUtils.isEmpty(productList)) {
+                return
+            }
+            val productIds = productList.map { it.id }
 
-        // 2. set bitmap
-        productIds.forEach { id ->
-            redisTemplate.opsForValue().setBit(RedisConstants.PRODUCT_BITMAP_KEY + id, id, true)
+            // 2. set bitmap
+            productIds.forEach { id ->
+                redisTemplate.opsForValue().setBit(RedisConstants.PRODUCT_BITMAP_KEY + id, id, true)
+            }
+            println("bitmap initialization successful")
+        } catch (ex: Exception) {
+            println("Error during bitmap initialization: ${ex.message}")
+            ex.printStackTrace()
         }
-        println("bitmap initialization successful")
     }
 }
